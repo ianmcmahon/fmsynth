8a4f6a2a5b5a0c3fa31791c2ad9b51fdd3ce7f43
McMahon, Ian <ian.mcmahon@delta.com>
2019-06-17 17:07:21 -0400

    UI coming together

--- ui/knob.go
+++ ui/knob.go
@@ -8,15 +8,18 @@ import (
 
 	"github.com/llgcode/draw2d"
 	"github.com/llgcode/draw2d/draw2dimg"
+	"github.com/llgcode/draw2d/draw2dkit"
 )
 
 var (
 	KNOB_SIZE = image.Rect(0, 0, 100, 120)
 
+	// from https://clrs.cc/
+	black   = color.RGBA{0x11, 0x11, 0x11, 0xff}
 	silver  = color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
 	fuschia = color.RGBA{0xf0, 0x12, 0xbe, 0xff}
 	yellow  = color.RGBA{0xff, 0xDC, 0x00, 0xff}
-	navy    = color.RGBA{0x00, 0x1F, 0x3F, 0xff}
+	navy    = color.RGBA{0x00, 0x1F, 0x3F, 0xaa}
 )
 
 // draws a filled rounded rect
@@ -60,17 +63,33 @@ func Knob(label string) *knob {
 	k.gc.SetStrokeColor(silver)
 	k.gc.SetLineWidth(1)
 
-	boxBackground(k.gc, k.Bounds())
+	draw2dkit.RoundedRectangle(k.gc,
+		float64(k.Bounds().Min.X+1), float64(k.Bounds().Min.Y+1),
+		float64(k.Bounds().Max.X-1), float64(k.Bounds().Max.Y-1),
+		10, 10)
+	k.gc.FillStroke()
 
-	k.gc.StrokeStringAt("Attack", 10, 10)
+	center := k.Bounds().Size().Div(2)
+
+	k.gc.SetFontData(draw2d.FontData{
+		Name:   "SFAlienEncountersSolid",
+		Family: draw2d.FontFamilySans,
+		Style:  draw2d.FontStyleItalic,
+	})
+	k.gc.SetFontSize(15)
+	k.gc.SetLineWidth(0.5)
+	k.gc.SetStrokeColor(black)
+	k.gc.SetFillColor(yellow)
+	l, _, r, _ := k.gc.GetStringBounds(k.label)
+	k.gc.FillStringAt(k.label, float64(center.X)-(r-l)/2.0, float64(k.Bounds().Max.Y-10))
+	k.gc.StrokeStringAt(k.label, float64(center.X)-(r-l)/2.0, float64(k.Bounds().Max.Y-10))
 
 	k.gc.SetStrokeColor(fuschia)
 	k.gc.SetLineWidth(6)
 	k.gc.SetLineCap(draw2d.RoundCap)
 	startAngle := 110.0 * (math.Pi / 180.0)
 	angle := 280.0 * (math.Pi / 180.0)
-	center := k.Bounds().Size().Div(2)
-	k.gc.ArcTo(float64(center.X), float64(center.Y), 30, 30, startAngle, angle)
+	k.gc.ArcTo(float64(center.X), float64(center.X), 35, 35, startAngle, angle)
 	k.gc.Stroke()
 
 	return k
--- ui/ui.go
+++ ui/ui.go
@@ -3,9 +3,31 @@ package ui
 import (
 	"image"
 	"image/draw"
+	"image/jpeg"
+	"os"
 
+	"github.com/llgcode/draw2d"
 	wde "github.com/skelterjohn/go.wde"
 )
+
+/*
+	UI Concepts
+
+	'screen' is the base canvas of the display
+	'layout' manages the background image and placement of child containers
+	'param page' contains eight individual param controls that map to hardware encoders
+	  as well as any background drawing it may do
+	a param control such as 'knob' knows how to draw its labels, indicators, and controls
+	  as well as any background drawing it may do
+	a Patch is a data structure that contains all of the params for the engine
+	a Param contains the concrete value of record, as well as metadata about how it
+	  maps to synth engine components, midi CCs, and UI elements, including label and font
+		for theming
+	param pages will be configured by paramId, so a given page can ask the patch for param
+	  metadata
+	it'd be possible to have user configurable param pages, or maybe just a macro page and
+	  a way to edit macros binding arbitrary parameters to them with attenuverting that all sum
+*/
 
 func Start() {
 	window, err := wde.NewWindow(800, 480)
@@ -17,13 +39,35 @@ func Start() {
 	window.LockSize(true)
 	window.Show()
 
-	page := image.NewRGBA(image.Rect(0, 0, 800, 480))
-	addLabel(page, 500, 300, "do words to it")
+	draw2d.SetFontCache(draw2d.NewFolderFontCache("ui/fonts"))
 
-	k := Knob("Turn Me")
-	draw.Draw(page, k.Bounds().Add(image.Pt(10, 10)), k, image.ZP, draw.Src)
+	screen := image.NewRGBA(image.Rect(0, 0, 800, 480))
 
-	window.Screen().CopyRGBA(page, page.Bounds())
+	backgroundFile, err := os.Open("ui/backgrounds/efe-kurnaz-315384-unsplash.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	backgroundImg, err := jpeg.Decode(backgroundFile)
+	if err != nil {
+		panic(err)
+	}
+
+	draw.Draw(screen, screen.Bounds(), backgroundImg, image.ZP, draw.Src)
+
+	page := image.NewRGBA(image.Rect(0, 0, KNOB_SIZE.Max.X*4, KNOB_SIZE.Max.Y*2))
+
+	knobs := []string{"ATTACC", "DECAY", "SUSTN", "RELEASE", "ALG", "FEEDBK", "MIX", "BKGRD"}
+	xSize := KNOB_SIZE.Max.X
+	ySize := KNOB_SIZE.Max.Y
+	for i := 0; i < 8; i++ {
+		k := Knob(knobs[i])
+		draw.Draw(page, k.Bounds().Add(image.Pt(i%4*xSize, i/4*ySize)), k, image.ZP, draw.Over)
+	}
+
+	draw.Draw(screen, page.Bounds().Add(image.Pt(20, 20)), page, image.ZP, draw.Over)
+
+	window.Screen().CopyRGBA(screen, screen.Bounds())
 	window.FlushImage()
 
 }
